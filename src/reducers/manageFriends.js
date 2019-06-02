3142e104da441d3b6d2e6193a88ae25c46cce5cc
Jack Hilscher <Jack@Jacks-MacBook-Air.local>
2019-06-02 18:07:09 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
 export function manageFriends(state, action){
+  switch (action.type){
+    case "ADD_FRIEND":
+      return {...state, friends: [...state.friends, action.friend]};
+    case "REMOVE_FRIEND":
+      const newFriends = state.friends.filter(friend => {
+        console.log(friend)
+        console.log(friend.id)
+        return friend.id !== action.id
+      })
+
+      return {...state, friends: newFriends};
+    default:
+      return state
+  }
 }
